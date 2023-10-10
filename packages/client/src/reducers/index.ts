06beab19892523de0be5b968c6cd03a5b78c06b4
Takayuki Ono <takaaaaadayo@gmail.com>
2023-10-10 17:39:01 +0530

    change filename and import to index.ts

--- packages/client/src/reducers/index.ts
+++ packages/client/src/reducers/index.ts
@@ -1,10 +1,18 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
 import abilities from './abilities';
-// import casuality from './casuality';
+import casualty from './casualty';
+import controlPanel from './controlPanel';
+import gameState from './gameState';
+import massage from './massage';
+import players from './players';
 
 export default combineReducers({
   auth,
   abilities,
-//   casuality,
+  casualty,
+  controlPanel,
+  gameState,
+  massage,
+  players,
 });
