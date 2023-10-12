4c3be5936cfa75397b0193c64956a5382f0c486f
Takayuki Ono <takaaaaadayo@gmail.com>
2023-10-12 16:15:14 +0530

    change to class type

--- packages/client/src/game/food.ts
+++ packages/client/src/game/food.ts
@@ -1,4 +1,4 @@
-import { scene, world, groundMaterial } from './main';
+import { scene, world, groundMaterial } from './game';
 import { myColors } from './config';
 import socket from '../socket';
 import store from '../store';
@@ -6,102 +6,134 @@ import store from '../store';
 import * as THREE from 'three';
 import * as CANNON from 'cannon';
 
-type FoodData = {
-    type: 'box' | 'moon' | 'sphere';
-    params: number[];
-    x: number;
-    y: number;
-    z: number;
-};
+import { FoodData } from '../types/DataTypes';
 
-export function createFood(id: string, data:FoodData): THREE.Mesh{
+interface CustomMesh extends THREE.Mesh {
+  cannon?: CANNON.Body;
+}
+
+interface CustomShape extends CANNON.Shape {
+    volume(): number;
+}
+
+interface CustomBody extends CANNON.Body {
+    shapes: CustomShape[];
+}
+
+interface CustomMesh extends THREE.Mesh {
+    cannon?: CustomBody;
+}
+
+export class Food {
+  private id: string;
+  private initialData: FoodData;
+  public mesh?: THREE.Mesh;
+  private eaten: boolean;
+  private color: string | null = null;
+
+  constructor(id: string, data: FoodData) {
+    this.id = id;
+    this.initialData = data;
+    this.eaten = false;
+  }
+
+  init(): void {
     const someColors = myColors();
     let count = 0;
 
-    const color = Object.keys(someColors).reduce((acc, prop) => {
+    for (const prop in someColors) {
       if (Math.random() < 1 / ++count) {
-        acc = prop;
+        this.color = prop;
       }
-      return acc;
-    }, "");
-
-    let geometry;
-    let material;
-    let shape;
-
-    const { type, params, x, y, z } = data;
-    switch(type) {
-        case "box":
-            geometry = new THREE.BoxGeometry(params[0], params[1], params[2]);
-            material = new THREE.MeshPhongMaterial({ color: someColors[color], flatShading: true });
-            shape = new CANNON.Box(new CANNON.Vec3(params[0] / 2, params[1] / 2, params[2] / 2));
-            break;
-        case "moon":
-            geometry = new THREE.IcosahedronGeometry(params[0],1);
-            material = new THREE.MeshPhongMaterial({ color: "#F8B195", flatShading: true });
-            shape = new CANNON.Sphere(params[0]);
-            break;
-        case "sphere":
-            default:
-                geometry = new THREE.TetrahedronGeometry(params[0], 1);
-                material = new THREE.MeshPhongMaterial({ color: someColors[color], flatShading: true });
-                shape = new CANNON.Sphere(params[0]);
-                break;
     }
 
-    const mesh = new THREE.Mesh(geometry, material);
-    mesh.name = id;
+    if (this.color == null) {
+      throw new Error("No color was selected for the food");
+    }
+
+    const { type, params, x, y, z } = this.initialData;
+    let geometry: THREE.BufferGeometry;
+    let material: THREE.Material;
+    let shape: CANNON.Shape;
+
+    switch (type) {
+      case 'box':
+        geometry = new THREE.BoxGeometry(params[0], params[1], params[2]);
+        material = new THREE.MeshPhongMaterial({ color: someColors[this.color],  flatShading: true });
+        shape = new CANNON.Box(new CANNON.Vec3(params[0] / 2, params[1] / 2, params[2] / 2));
+        break;
+      case 'moon':
+        geometry = new THREE.IcosahedronGeometry(params[0], 1);
+        material = new THREE.MeshPhongMaterial({ color: "#F8B195",  flatShading: true });
+        shape = new CANNON.Sphere(params[0]);
+        break;
+      case 'sphere':
+      default:
+        geometry = new THREE.TetrahedronGeometry(params[0], 1);
+        material = new THREE.MeshPhongMaterial({ color: someColors[this.color],  flatShading: true });
+        shape = new CANNON.Sphere(params[0]);
+    }
+
+    const mesh: CustomMesh = new THREE.Mesh(geometry, material);
+
+    if(mesh === undefined) {
+      throw new Error("Mesh is undefined");
+    }
+
+    mesh.name = this.id;
     mesh.castShadow = true;
     mesh.position.set(x, y, z);
 
-    if(type === "moon") {
-        mesh.position.normalize().multiplyScalar(500);
+    if (type === "moon") {
+      mesh.position.normalize().multiplyScalar(600);
     } else {
-        mesh.position.normalize().multiplyScalar(400);
+      mesh.position.normalize().multiplyScalar(500);
     }
-    mesh.position.add(mesh.position.clone().normalize().multiplyScalar(params[0] * 2.5));
 
+    mesh.position.add(mesh.position.clone().normalize().multiplyScalar(params[0] * 2.5));
     mesh.lookAt(new THREE.Vector3(0, 0, 0));
 
-    const cannonBody = new CANNON.Body({
-        shape, mass: 0, material: groundMaterial
-    });
-    cannonBody.position.set(mesh.position.x, mesh.position.y, mesh.position.z);
-    cannonBody.quaternion.set(-mesh.quaternion.x, -mesh.quaternion.y, -mesh.quaternion.z, mesh.quaternion.w);
+    mesh["cannon"] = new CANNON.Body({ shape, mass: 0, material: groundMaterial });
+    mesh["cannon"].position.set(mesh.position.x, mesh.position.y, mesh.position.z);
+    mesh["cannon"].quaternion.set(-mesh.quaternion.x, -mesh.quaternion.y, -mesh.quaternion.z, mesh.quaternion.w);
 
-    if(type === "moon"){
-        cannonBody.collisionResponse = true;
+    if (type === "moon") {
+      mesh["cannon"].collisionResponse = true;
     } else {
-        cannonBody.collisionResponse = false;
+      mesh["cannon"].collisionResponse = false;
     }
 
-    cannonBody.addEventListener('collide', e => {
-        const player = scene.getObjectByName(socket.id);
-        if (!mesh.userData.eaten && player) {
-          for (const contact of world.contacts) {
-            const foodHits = contact.bi === cannonBody;
-            const playerIsHit = contact.bj === (player as any).cannon;
-            const playerHits = contact.bi === (player as any).cannon;
-            const foodIsHit = contact.bj === cannonBody;
-            if (foodHits && playerIsHit || playerHits && foodIsHit) {
-              const playerVol = store.getState().players[socket.id].volume;
-              const foodVol = cannonBody.shapes[0].volume();
-    
-              if (playerVol > foodVol) {
-                mesh.userData.eaten = true;
-                socket.emit('eat_food', id, foodVol + playerVol);
-              }
+    mesh["cannon"].addEventListener('collide', (e: any) => {
+      const player = scene.getObjectByName(socket.id) as any;
+
+      if (!this.eaten && player) {
+        for (const contact of world.contacts) {
+          const foodHits = contact.bi === mesh["cannon"];
+          const playerIsHit = contact.bj === player.cannon;
+          const playerHits = contact.bi === player.cannon;
+          const foodIsHit = contact.bj === mesh["cannon"];
+
+          if (foodHits && playerIsHit || playerHits && foodIsHit) {
+            const playerVol = store.getState().players[socket.id].volume;
+            const foodVol = (mesh["cannon"] as CANNON.Body).shapes[0].volume();
+
+            if (playerVol > foodVol) {
+              this.eaten = true;
+              socket.emit('eat_food', this.id, foodVol + playerVol);
             }
           }
         }
-      });
-    
-      scene.add(mesh);
-      world.add(cannonBody);
-      mesh.userData.cannon = cannonBody;
-    
-      return mesh;
+      }
+    });
+
+    scene.add(mesh);
+    world.add(mesh["cannon"]);
+
+    this.mesh = mesh;
+  }
 }
+
+export let controls: any; 
 
 
 
