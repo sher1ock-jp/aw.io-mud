0adf88f4e55a4ea2a8fccf4b3d94941e601b8787
Takayuki Ono <takaaaaadayo@gmail.com>
2023-10-12 16:38:52 +0530

    add food/player colors

--- packages/client/src/game/config.ts
+++ packages/client/src/game/config.ts
@@ -7,18 +7,24 @@ type ColorPalette = {
     [key: string]: string | undefined; 
 };
 
-export const myColors = (): ColorPalette => {
-    const colArray: ColorPalette[] = [
-        {
-            grey: '#556270',
-            green: '#C7F464',
-            blue: '#4ECDC4',
-            pink: '#FF6B6B',
-            red: '#C44D58',
-        },
-    ];
+export const foodColors = (): ColorPalette => {
+    return {
+        grey: '#556270',
+        green: '#C7F464',
+        blue: '#4ECDC4',
+        pink: '#FF6B6B',
+        red: '#C44D58',
+    };
+};
 
-    return colArray[0];
+export const playerColors = (): ColorPalette => {
+    // プレイヤー用の色をここで定義
+    return {
+        grey: '#A1A1A1',
+        green: '#88F234',
+        blue: '#1234AB',
+        // その他の色
+    };
 };
 
 export const fixedTimeStep: number = 1.0 / 30.0;
